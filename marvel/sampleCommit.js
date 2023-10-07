5eac56ddc68a32c6df4b8d87c837f9b17f559b35
john roy necesario <140679532+jnecesario318@users.noreply.github.com>
2023-10-07 09:48:46 -0700

    Add files via upload

---
+++ marvel/sampleCommit.js
@@ -0,0 +1,16 @@
+ // JavaScript functions for the calculator
+ function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
